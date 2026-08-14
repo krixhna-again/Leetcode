988f38cad2832139d9ee57c6384bffba02d612f7
Krishna <krishna0017k@gmail.com>
2026-08-14 17:32:14 +0530

    Time: 46 ms (33.51%), Space: 52.8 MB (88.81%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,7 +1,6 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
 Array.prototype.last = function() {
-    if (this.length === 0) {
-        return -1;
-    }
-
-    return this[this.length - 1];
+    return this.length ? this[this.length - 1] : -1;
 };
