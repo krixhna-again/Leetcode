10d6e7b699bd561227db59dbf183cdf130869553
Krishna <krishna0017k@gmail.com>
2026-09-04 18:43:42 +0530

    Time: 0 ms (100%), Space: 55.7 MB (92.59%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.js
@@ -0,0 +1,22 @@
+var firstStableIndex = function(nums, k) {
+    let n = nums.length;
+    let suffixMin = new Array(n);
+
+    suffixMin[n - 1] = nums[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+    }
+
+    let prefixMax = nums[0];
+
+    for (let i = 0; i < n; i++) {
+        prefixMax = Math.max(prefixMax, nums[i]);
+
+        if (prefixMax - suffixMin[i] <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+};
